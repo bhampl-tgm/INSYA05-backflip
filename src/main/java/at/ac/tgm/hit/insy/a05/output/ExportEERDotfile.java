edcda2a1e05af1c235a8b5689a18bdc40831837a
Burkhard Hampl <bhampl@student.tgm.ac.at>
2015-01-16 19:50:42 +0100

    Started to implement ExportEERDotfile

---
+++ src/main/java/at/ac/tgm/hit/insy/a05/output/ExportEERDotfile.java
@@ -0,0 +1,84 @@
+package at.ac.tgm.hit.insy.a05.output;
+
+import at.ac.tgm.hit.insy.a05.structur.Attribut;
+import at.ac.tgm.hit.insy.a05.structur.Database;
+import at.ac.tgm.hit.insy.a05.structur.Table;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class ExportEERDotfile implements Exportable {
+
+    private PrintWriter output;
+    private Database database;
+
+    private final static String startER = "graph ER {";
+
+    private final static String endER = "}";
+
+    private final static String tables = "\tnode [shape=box]; ";
+
+    private final static String attributes = "\tnode [shape=ellipse]; ";
+
+    private final static String startNodeLable = "\t\t{node [label=\"";
+
+    private final static String endLable = "\"] ";
+
+    private final static String endNode = "}";
+
+    private final static String endAttribute = "; ";
+
+    @Override
+    public void export(Database database, File file) throws FileNotFoundException {
+        this.database = database;
+        this.output = new PrintWriter(file);
+        this.output.println(ExportEERDotfile.startER);
+        this.printTables();
+        this.printAttributes();
+
+        this.output.println(ExportEERDotfile.endER);
+    }
+
+    private void printAttributes() {
+        this.output.println(ExportEERDotfile.attributes);
+        for (Table table : database.getTables()) {
+            for (Attribut attribut : table.getPrimaryKeys()) {
+                //TODO PK underline
+                this.output.print(ExportEERDotfile.startNodeLable);
+                this.output.print(attribut.getName());
+                this.output.print(ExportEERDotfile.endLable);
+                this.output.print(table.getName());
+                this.output.print(attribut.getName());
+                this.output.print(ExportEERDotfile.endAttribute);
+                this.output.println(ExportEERDotfile.endNode);
+            }
+
+            for (Attribut attribut : table.getAttributes()) {
+                this.output.print(ExportEERDotfile.startNodeLable);
+                this.output.print(attribut.getName());
+                this.output.print(ExportEERDotfile.endLable);
+                this.output.print(table.getName());
+                this.output.print(attribut.getName());
+                this.output.print(ExportEERDotfile.endAttribute);
+                this.output.println(ExportEERDotfile.endNode);
+            }
+        }
+
+    }
+
+    private void printTables() {
+        this.output.print(ExportEERDotfile.tables);
+        for (Table table : database.getTables()) {
+            this.output.print(table.getName());
+            this.output.print(ExportEERDotfile.endAttribute);
+        }
+        this.output.println();
+    }
+
+
+    @Override
+    public void close() {
+        this.output.close();
+    }
+}
--- src/main/java/at/ac/tgm/hit/insy/a05/output/ExportRMTXT.java
+++ src/main/java/at/ac/tgm/hit/insy/a05/output/ExportRMTXT.java
@@ -5,14 +5,10 @@ import at.ac.tgm.hit.insy.a05.structur.Database;
 import java.io.File;
 import java.io.PrintWriter;
 
-public class ExportRMDotfile implements Exportable {
+public class ExportRMTXT implements Exportable {
 
 
     private PrintWriter output;
-
-    private final static String startER = "graph ER {\n";
-
-    private final static String endER = "graph ER {\n";
 
     @Override
     public void export(Database database, File file) {
