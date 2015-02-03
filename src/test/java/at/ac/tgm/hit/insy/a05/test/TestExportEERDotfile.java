6cccc9c5961da64c269d6a0398a94d0720d7da03
Burkhard Hampl <bhampl@student.tgm.ac.at>
2015-02-03 16:09:29 +0100

    Finished Test for ExportRMHTML and ExportEERDotfile

--- src/test/java/at/ac/tgm/hit/insy/a05/test/TestExportEERDotfile.java
+++ src/test/java/at/ac/tgm/hit/insy/a05/test/TestExportEERDotfile.java
@@ -21,6 +21,13 @@ import static org.junit.Assert.assertTrue;
  */
 public class TestExportEERDotfile {
 
+    private static final String startPK = "<<u>";
+    private static final String endPK = "</u>>";
+    private static final String notnull = "&lt;NOT NULL&gt;";
+    private static final String seperator = " -- ";
+    private static final String startCardinality = " [label=\"";
+    private static final String endCardinality = "\"];";
+
     private Database database;
 
     @Before
@@ -28,20 +35,30 @@ public class TestExportEERDotfile {
         this.database = new Database("testDatabase");
         Table planes = new Table("planes");
         planes.addPrimaryKey(new Attribute("id", planes));
-        planes.addAttribute(new Attribute("seats", planes));
+        planes.addPrimaryKey(new Attribute("alternativeid", planes));
+        Attribute seatsAttribute = new Attribute("seats", planes);
+        seatsAttribute.setUnique(true);
+        planes.addAttribute(seatsAttribute);
 
         Table airlines = new Table("airlines");
         airlines.addPrimaryKey(new Attribute("id", airlines));
-        airlines.addAttribute(new Attribute("country", airlines));
+        Attribute countryAttribute = new Attribute("country", airlines);
+        countryAttribute.setNotNull(true);
+        airlines.addAttribute(countryAttribute);
 
         Table flights = new Table("flights");
         flights.addPrimaryKey(new Attribute("nr", flights));
         Attribute airlinePrimary = new Attribute("airline", flights);
         airlinePrimary.setReference(new Reference(airlines, airlines.getPrimaryKey("id")));
         Attribute planeAttribute = new Attribute("plane", flights);
+        planeAttribute.setUnique(true);
         planeAttribute.setReference(new Reference(planes, planes.getPrimaryKey("id")));
+        Attribute planeAttribute1 = new Attribute("plane1", flights);
+        planeAttribute1.setNotNull(true);
+        planeAttribute1.setReference(new Reference(planes, planes.getPrimaryKey("id")));
         flights.addPrimaryKey(airlinePrimary);
         flights.addAttribute(planeAttribute);
+        flights.addAttribute(planeAttribute1);
 
         this.database.addTable(planes);
         this.database.addTable(airlines);
@@ -85,16 +102,115 @@ public class TestExportEERDotfile {
     }
 
     @Test
-    public void testAttributes() throws IOException {
-        new ExportEERDotfile().export(this.database, new File(TestMain.TESTPATH + "test.html"));
-        BufferedReader file = new BufferedReader(new FileReader(TestMain.TESTPATH + "test.html"));
+    public void testAttribute() throws IOException {
+        new ExportEERDotfile().export(this.database, new File(TestMain.TESTPATH + "test.dot"));
+        BufferedReader file = new BufferedReader(new FileReader(TestMain.TESTPATH + "test.dot"));
+        String line = file.readLine();
+        while (!line.contains("ellipse")) {
+            line = file.readLine();
+        }
+        file.readLine();
+        file.readLine();
+        line = file.readLine();
+        assertTrue(line.contains("seats"));
+    }
+
+    @Test
+    public void testPrimaryKey() throws IOException {
+        new ExportEERDotfile().export(this.database, new File(TestMain.TESTPATH + "test.dot"));
+        BufferedReader file = new BufferedReader(new FileReader(TestMain.TESTPATH + "test.dot"));
         String line = file.readLine();
         while (!line.contains("ellipse")) {
             line = file.readLine();
         }
         line = file.readLine();
-        assertTrue(line.contains("id"));
+        assertTrue(line.contains("id" + TestExportEERDotfile.endPK));
+        assertTrue(line.contains(TestExportEERDotfile.startPK));
     }
 
+    @Test
+    public void testNotNull() throws IOException {
+        new ExportEERDotfile().export(this.database, new File(TestMain.TESTPATH + "test.dot"));
+        BufferedReader file = new BufferedReader(new FileReader(TestMain.TESTPATH + "test.dot"));
+        String line = file.readLine();
+        while (!line.contains("ellipse")) {
+            line = file.readLine();
+        }
+        for (int i = 0; i < 4; i++) {
+            file.readLine();
+        }
+        line = file.readLine();
+        assertTrue(line.contains("country" + TestExportEERDotfile.notnull));
+    }
 
+    @Test
+    public void testRelation() throws IOException {
+        new ExportEERDotfile().export(this.database, new File(TestMain.TESTPATH + "test.dot"));
+        BufferedReader file = new BufferedReader(new FileReader(TestMain.TESTPATH + "test.dot"));
+        String line = file.readLine();
+        while (!line.contains("diamond")) {
+            line = file.readLine();
+        }
+        line = file.readLine();
+        assertTrue(line.contains("flights" + "airline" + "airlines" + "id"));
+    }
+
+    @Test
+    public void testAttributeToTable() throws IOException {
+        new ExportEERDotfile().export(this.database, new File(TestMain.TESTPATH + "test.dot"));
+        BufferedReader file = new BufferedReader(new FileReader(TestMain.TESTPATH + "test.dot"));
+        String line = file.readLine();
+        while (!line.contains("diamond")) {
+            line = file.readLine();
+        }
+        file.readLine();
+        file.readLine();
+        line = file.readLine();
+        assertTrue(line.contains("id" + TestExportEERDotfile.seperator + "planes"));
+    }
+
+    @Test
+    public void testTableToRelation() throws IOException {
+        new ExportEERDotfile().export(this.database, new File(TestMain.TESTPATH + "test.dot"));
+        BufferedReader file = new BufferedReader(new FileReader(TestMain.TESTPATH + "test.dot"));
+        String line = file.readLine();
+        while (!line.contains("diamond")) {
+            line = file.readLine();
+        }
+        for (int i = 0; i < 8; i++) {
+            file.readLine();
+        }
+        line = file.readLine();
+        assertTrue(line.contains("flights" + TestExportEERDotfile.seperator + "flights" + "airline" + "airlines" + "id"));
+    }
+
+    @Test
+    public void testCardinalityi_N() throws IOException {
+        new ExportEERDotfile().export(this.database, new File(TestMain.TESTPATH + "test.dot"));
+        BufferedReader file = new BufferedReader(new FileReader(TestMain.TESTPATH + "test.dot"));
+        String line = file.readLine();
+        while (!line.contains("diamond")) {
+            line = file.readLine();
+        }
+        for (int i = 0; i < 8; i++) {
+            file.readLine();
+        }
+        line = file.readLine();
+        assertTrue(line.contains("flights" + "airline" + "airlines" + "id" + TestExportEERDotfile.startCardinality + "n" + TestExportEERDotfile.endCardinality));
+    }
+
+    @Test
+    public void testCardinality_1() throws IOException {
+        new ExportEERDotfile().export(this.database, new File(TestMain.TESTPATH + "test.dot"));
+        BufferedReader file = new BufferedReader(new FileReader(TestMain.TESTPATH + "test.dot"));
+        String line = file.readLine();
+        while (!line.contains("diamond")) {
+            line = file.readLine();
+        }
+        for (int i = 0; i < 9; i++) {
+            file.readLine();
+        }
+        line = file.readLine();
+        assertTrue(line.contains("flights" + "airline" + "airlines" + "id" + TestExportEERDotfile.startCardinality + "1" + TestExportEERDotfile.endCardinality));
+    }
 } 
--- src/test/java/at/ac/tgm/hit/insy/a05/test/TestExportRMHTML.java
+++ src/test/java/at/ac/tgm/hit/insy/a05/test/TestExportRMHTML.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 
 import java.io.*;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 
 /**
@@ -28,37 +28,50 @@ public class TestExportRMHTML {
 
     private static final String endAttribute = ", ";
 
+    private static final String unique = "&lt;UNIQUE&gt;";
+
+    private static final String notnull = "&lt;NOT NULL&gt;";
+
     private Database database;
 
     /**
      * Flugzeug: Nummer
-     *           sitzplaetze
+     * sitzplaetze
      * Flight: nr
-     *         Primary foreign airline id
-     *         Attribut Flugzeug id
+     * Primary foreign airline id
+     * Attribut Flugzeug id
      * Airline: id
-     *          land
+     * land
      */
 
     @Before
     public void initialize() {
         this.database = new Database("testDatabase");
-        Table planes =  new Table("planes");
+        Table planes = new Table("planes");
         planes.addPrimaryKey(new Attribute("id", planes));
-        planes.addAttribute(new Attribute("seats", planes));
+        Attribute seatsAttribute = new Attribute("seats", planes);
+        seatsAttribute.setUnique(true);
+        planes.addAttribute(seatsAttribute);
 
         Table airlines = new Table("airlines");
         airlines.addPrimaryKey(new Attribute("id", airlines));
-        airlines.addAttribute(new Attribute("country",airlines));
+        Attribute countryAttribute = new Attribute("country", airlines);
+        countryAttribute.setNotNull(true);
+        airlines.addAttribute(countryAttribute);
 
         Table flights = new Table("flights");
         flights.addPrimaryKey(new Attribute("nr", flights));
         Attribute airlinePrimary = new Attribute("airline", flights);
         airlinePrimary.setReference(new Reference(airlines, airlines.getPrimaryKey("id")));
         Attribute planeAttribute = new Attribute("plane", flights);
+        planeAttribute.setUnique(true);
         planeAttribute.setReference(new Reference(planes, planes.getPrimaryKey("id")));
+        Attribute planeAttribute1 = new Attribute("plane1", flights);
+        planeAttribute1.setNotNull(true);
+        planeAttribute1.setReference(new Reference(planes, planes.getPrimaryKey("id")));
         flights.addPrimaryKey(airlinePrimary);
         flights.addAttribute(planeAttribute);
+        flights.addAttribute(planeAttribute1);
 
         this.database.addTable(planes);
         this.database.addTable(airlines);
@@ -69,10 +82,10 @@ public class TestExportRMHTML {
     @Test
     public void testFileExists() throws FileNotFoundException {
         new ExportRMHTML().export(this.database, new File(TestMain.TESTPATH + "test.html"));
-        assertTrue(new FileReader(TestMain.TESTPATH + "test.html").toString()!=null);
+        assertTrue(new FileReader(TestMain.TESTPATH + "test.html").toString() != null);
     }
 
-    @Test(expected=FileNotFoundException.class)
+    @Test(expected = FileNotFoundException.class)
     public void testFileNotExists() throws FileNotFoundException {
         new ExportRMHTML().export(this.database, new File(""));
     }
@@ -139,4 +152,30 @@ public class TestExportRMHTML {
         line = file.readLine();
         assertTrue(line.contains(beginPK + beginFK + "airline:airlines.id" + endFK + endPK));
     }
+
+    @Test
+    public void testUnique() throws IOException {
+        new ExportRMHTML().export(this.database, new File(TestMain.TESTPATH + "test.html"));
+        BufferedReader file = new BufferedReader(new FileReader(TestMain.TESTPATH + "test.html"));
+        String line = file.readLine();
+        while (!line.contains("<br />")) {
+            line = file.readLine();
+        }
+        file.readLine();
+        line = file.readLine();
+        assertTrue(line.contains(endAttribute + beginFK + "plane:planes.id" + endFK + unique));
+    }
+
+    @Test
+    public void testNotNull() throws IOException {
+        new ExportRMHTML().export(this.database, new File(TestMain.TESTPATH + "test.html"));
+        BufferedReader file = new BufferedReader(new FileReader(TestMain.TESTPATH + "test.html"));
+        String line = file.readLine();
+        while (!line.contains("<br />")) {
+            line = file.readLine();
+        }
+        line = file.readLine();
+        assertTrue(line.contains(endAttribute + "country" + notnull));
+    }
+
 }
